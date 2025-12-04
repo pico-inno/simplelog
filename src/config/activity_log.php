a90cdce6df216c79f864cc7a7f7f30494a93044b
Zai185 <mkhant185@gmail.com>
2025-12-04 14:48:26 +0700

    add transaction on logging with batch

--- src/config/activity_log.php
+++ src/config/activity_log.php
@@ -15,8 +15,18 @@ return [
     | DEFAULT LOG NAME FOR LOGGGING
     |--------------------------------------------------------------------------
 
-    | In order to log for every model using SimpleLog trait, this must be marked as true
+    | This log name will be used in case there is no name for activity() method on logging
     */
     'log_name' => env('DEFAULT_LOGNAME', 'default'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | ENABLE DB TRANSACTION & ROLLBACK WITH LOGGING
+    |--------------------------------------------------------------------------
+
+    | In order to use DB transaction & rollback on every log, this must be marked as true
+    */
+    'db_transaction_on_log' => env('DB_TRANSACTION_ON_LOG', false),
+
 
 ];
